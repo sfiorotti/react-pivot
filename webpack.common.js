084573d9ee4646a1863cb94464553e03ebd2c07f
Sergio Fiorotti <s.fiorotti@hotmail.com>
2019-08-31 20:12:04 -0300

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'finkraft.bundle.js',
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, 'app')
+    ],
+    extensions: [".js", ".json", ".css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[name].[ext]',
+              limit: false,
+            },
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+};
